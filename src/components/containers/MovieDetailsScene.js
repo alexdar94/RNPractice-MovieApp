59d94d08948d2da1883a8305ae10c04348c61d23
Alex Lee <alexdar94@gmail.com>
2017-08-23 18:32:35 +0800

    Add react-native-video-controls library

--- src/components/containers/MovieDetailsScene.js
+++ src/components/containers/MovieDetailsScene.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text, Dimensions} from 'react-native';
 import Video from 'react-native-video';
+import VideoPlayer from 'react-native-video-controls';
 
 class MovieDetailsScene extends Component {
 
@@ -8,22 +9,23 @@ class MovieDetailsScene extends Component {
 
     renderVideoView(source, onEndCallback) {
         return (
-            <Video source={source}   // Can be a URL or a local file.
-                   ref={(ref) => {
-                       this.player = ref
-                   }}                                      // Store reference
-                   rate={1.0}                              // 0 is paused, 1 is normal.
-                   volume={1.0}                            // 0 is muted, 1 is normal.
-                   muted={false}                           // Mutes the audio entirely.
-                   paused={false}                          // Pauses playback entirely.
-                   resizeMode="contain"                      // Fill the whole screen at aspect ratio.*
-                   repeat={false}                           // Repeat forever.
-                   playInBackground={false}                // Audio continues to play when app entering background.
-                   playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
-                   ignoreSilentSwitch={"ignore"}           // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent. When 'obey', audio will toggle with the switch. When not specified, will inherit audio settings as usual.
-                   progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
-                   onEnd={onEndCallback.bind(this)}
-                   style={styles.backgroundVideo}/>
+            <VideoPlayer source={source}   // Can be a URL or a local file.
+                         ref={(ref) => {
+                             this.player = ref
+                         }}                                      // Store reference
+                         rate={1.0}                              // 0 is paused, 1 is normal.
+                         volume={1.0}                            // 0 is muted, 1 is normal.
+                         muted={false}                           // Mutes the audio entirely.
+                         paused={false}                          // Pauses playback entirely.
+                         resizeMode="contain"                      // Fill the whole screen at aspect ratio.*
+                         repeat={false}                           // Repeat forever.
+                         playInBackground={false}                // Audio continues to play when app entering background.
+                         playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
+                         ignoreSilentSwitch={"ignore"}           // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent. When 'obey', audio will toggle with the switch. When not specified, will inherit audio settings as usual.
+                         progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
+                         onEnd={onEndCallback.bind(this)}
+                         navigator={this.props.navigator}
+                         style={styles.backgroundVideo}/>
         );
     }
 
@@ -41,11 +43,14 @@ class MovieDetailsScene extends Component {
 
     render() {
         return (
-            //{uri: this.props.movie.link[1].attributes.href}
+            //{this.state.startVideo ? this.renderVideoView({uri: this.props.movie.link[1].attributes.href}, this.onVideoEnd) : null}
             <View style={{flex: 1}}>
-                {this.state.startPostRoll ? this.renderVideoView(require('../../resources/video.mp4'), this.onPostRollEnd) : null}
-                {this.state.startVideo ? this.renderVideoView(require('../../resources/video.mp4'), this.onVideoEnd) : null}
-                {this.state.startPreRoll ? this.renderVideoView(require('../../resources/video.mp4'), this.onPreRollEnd) : null}
+                <View style={{height: Dimensions.get('window').width * (9 / 16), backgroundColor: 'black'}}>
+                    {this.state.startPostRoll ? this.renderVideoView(require('../../resources/video.mp4'), this.onPostRollEnd) : null}
+                    {this.state.startVideo ? this.renderVideoView({uri: this.props.movie.link[1].attributes.href}, this.onVideoEnd) : null}
+                    {this.state.startPreRoll ? this.renderVideoView(require('../../resources/video.mp4'), this.onPreRollEnd) : null}
+                </View>
+                <View style={{flex: 1, background: 'white'}}/>
             </View>
         );
     }
@@ -58,7 +63,15 @@ const styles = StyleSheet.create({
         left: 0,
         bottom: 0,
         right: 0,
+        height: Dimensions.get('window').width * (9 / 16),
+        backgroundColor: 'black'
     },
+    textStyle: {
+        zIndex: 1,
+        fontSize: 20,
+        position: 'absolute',
+        alignSelf: 'flex-end'
+    }
 });
 
 export default MovieDetailsScene;
