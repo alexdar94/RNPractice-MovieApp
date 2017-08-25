51a49147218c20ca56c611e3734bdefad52df300
Alex Lee <alexdar94@gmail.com>
2017-08-25 17:13:40 +0800

    Apply video player and ad player

--- src/components/containers/MovieDetailsScene.js
+++ src/components/containers/MovieDetailsScene.js
@@ -1,31 +1,56 @@
 import React, {Component} from 'react';
 import {View, StyleSheet, Text, Dimensions} from 'react-native';
 import VideoPlayer from 'react-native-video-controls';
+import AdPlayer from '../AdPlayer/AdPlayer';
+import {Actions} from 'react-native-router-flux';
 
 class MovieDetailsScene extends Component {
 
     state = {startPreRoll: true, startVideo: false, startPostRoll: false};
 
-    renderVideoView(source, onEndCallback, isAd) {
+    renderAdView(source, onEndCallback, onVisitAdvertiser = this.onVisitAdvertiser) {
+        return (
+            <AdPlayer source={source}   // Can be a URL or a local file.
+                      ref={(ref) => {
+                          this.player = ref
+                      }}                                      // Store reference
+                      rate={1.0}                              // 0 is paused, 1 is normal.
+                      volume={1.0}                            // 0 is muted, 1 is normal.
+                      muted={false}                           // Mutes the audio entirely.
+                      paused={false}                          // Pauses playback entirely.
+                      resizeMode="contain"                      // Fill the whole screen at aspect ratio.*
+                      repeat={false}                           // Repeat forever.
+                      playInBackground={false}                // Audio continues to play when app entering background.
+                      playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
+                      ignoreSilentSwitch={"ignore"}           // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent. When 'obey', audio will toggle with the switch. When not specified, will inherit audio settings as usual.
+                      progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
+                      onEnd={onEndCallback.bind(this)}
+                      onVisitAdvertiser={onVisitAdvertiser.bind(this)}
+                      navigator={this.props.navigator}
+                      style={styles.backgroundVideo}
+                      preview={{uri: this.props.movie['im:image'][2].label}}/>
+        );
+    }
+
+    renderVideoView(source, onEndCallback) {
         return (
             <VideoPlayer source={source}   // Can be a URL or a local file.
-                         ref={(ref) => {
-                             this.player = ref
-                         }}                                      // Store reference
-                         rate={1.0}                              // 0 is paused, 1 is normal.
-                         volume={1.0}                            // 0 is muted, 1 is normal.
-                         muted={false}                           // Mutes the audio entirely.
-                         paused={false}                          // Pauses playback entirely.
-                         resizeMode="contain"                      // Fill the whole screen at aspect ratio.*
-                         repeat={false}                           // Repeat forever.
-                         playInBackground={false}                // Audio continues to play when app entering background.
-                         playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
-                         ignoreSilentSwitch={"ignore"}           // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent. When 'obey', audio will toggle with the switch. When not specified, will inherit audio settings as usual.
-                         progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
-                         onEnd={onEndCallback.bind(this)}
-                         navigator={this.props.navigator}
-                         isAd={isAd}
-                         style={styles.backgroundVideo}/>
+                      ref={(ref) => {
+                          this.player = ref
+                      }}                                      // Store reference
+                      rate={1.0}                              // 0 is paused, 1 is normal.
+                      volume={1.0}                            // 0 is muted, 1 is normal.
+                      muted={false}                           // Mutes the audio entirely.
+                      paused={false}                          // Pauses playback entirely.
+                      resizeMode="contain"                      // Fill the whole screen at aspect ratio.*
+                      repeat={false}                           // Repeat forever.
+                      playInBackground={false}                // Audio continues to play when app entering background.
+                      playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
+                      ignoreSilentSwitch={"ignore"}           // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent. When 'obey', audio will toggle with the switch. When not specified, will inherit audio settings as usual.
+                      progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
+                      onEnd={onEndCallback.bind(this)}
+                      navigator={this.props.navigator}
+                      style={styles.backgroundVideo}/>
         );
     }
 
@@ -41,14 +66,17 @@ class MovieDetailsScene extends Component {
         this.setState({startPostRoll: false});
     }
 
+    onVisitAdvertiser() {
+        Actions.visitAdvertiser();
+    }
+
     render() {
         return (
-            //{this.state.startVideo ? this.renderVideoView({uri: this.props.movie.link[1].attributes.href}, this.onVideoEnd) : null}
             <View style={{flex: 1}}>
                 <View style={{height: Dimensions.get('window').width * (9 / 16), backgroundColor: 'black'}}>
-                    {this.state.startPostRoll ? this.renderVideoView(require('../../resources/video.mp4'), this.onPostRollEnd, false) : null}
-                    {this.state.startVideo ? this.renderVideoView({uri: this.props.movie.link[1].attributes.href}, this.onVideoEnd, false) : null}
-                    {this.state.startPreRoll ? this.renderVideoView(require('../../resources/video.mp4'), this.onPreRollEnd, true) : null}
+                    {this.state.startPreRoll ? this.renderAdView({uri: `http${this.props.movie.link[1].attributes.href.substring(5)}`}, this.onPreRollEnd) : null}
+                    {this.state.startVideo ? this.renderVideoView({uri: `http${this.props.movie.link[1].attributes.href.substring(5)}`}, this.onVideoEnd) : null}
+                    {this.state.startPostRoll ? this.renderAdView(require('../../resources/video.mp4'), this.onPostRollEnd) : null}
                 </View>
             </View>
         );
@@ -63,14 +91,7 @@ const styles = StyleSheet.create({
         bottom: 0,
         right: 0,
         height: Dimensions.get('window').width * (9 / 16),
-        backgroundColor: 'black'
     },
-    textStyle: {
-        zIndex: 1,
-        fontSize: 20,
-        position: 'absolute',
-        alignSelf: 'flex-end'
-    }
 });
 
 export default MovieDetailsScene;
